980addf2d7c29460cdc5405bf9a4abec278c0296
Evan Weaver <eweaver@twitter.com>
2010-12-06 05:15:20 -0800

    fix

--- blog_importer.rb
+++ blog_importer.rb
@@ -4,6 +4,9 @@ require "fileutils"
 require "erb"
 require "time"
 
+GMT_OFFSET =  60*60*8
+DATE_FORMAT = "%Y-%m-%d %H:%M"
+
 header = ERB.new(File.read("header.erb"))
 post = ERB.new(File.read("post.erb"))
 comment = ERB.new(File.read("comment.erb"))
@@ -16,34 +19,37 @@ FileUtils.chdir("import") do
       url = path.split("/")
       @post_id += 1
       @post_short_link = url[-2]
-      @post_date = url[0..2].join("/")
-      @post_link = "http://blog.evanweaver.com/" + @post_date + "/" + @post_short_link + "/"
-      @post_date_gmt = Time.parse(@post_date).strftime("%Y-%m-%d %H:%M")
+      @post_date_path = url[0..2].join("/")
+      @post_link = "http://blog.evanweaver.com/" + @post_date_path + "/" + @post_short_link + "/"
+      date = Time.parse(@post_date_path)
+      @post_date = date.strftime(DATE_FORMAT)
+      @post_date_gmt = (date + GMT_OFFSET).strftime(DATE_FORMAT)
       @post_excerpt = File.read(path)
-      @post_title = File.read(path.sub("intro", "title"))
-      @post_contents = @post_excerpt + File.read(path.sub("intro", "body"))
+      @post_title = File.read(path.sub("intro.el", "title.el"))
+      @post_contents = @post_excerpt + File.read(path.sub("intro.el", "body.el"))
       @post_excerpt = nil if @post_contents == @post_excerpt
       @post_comments = ""
       @comment_id = 0
 
-      Dir["#{@post_date}/#{@post_short_link}/comments/**/body.element"].sort.each do |path|
+      Dir["#{@post_date_path}/#{@post_short_link}/comments/**/body.element"].sort.each do |path|
         @comment_id += 1
-        author = File.read(path.sub("body", "author"))
+        author = File.read(path.sub("body.el", "author.el"))
         if author =~ /href="(.*?)" rel="nofollow">(.*?)</
           @comment_author = $2
           @comment_author_url = $1
         else
           @comment_author = author
         end
-
-        @comment_date_gmt = Time.parse(File.read(path.sub("body", "date"))).strftime("%Y-%m-%d %H:%M")
+        
+        date = Time.parse(File.read(path.sub("body.el", "date.el")))
+        @comment_date = date.strftime(DATE_FORMAT)
+        @comment_date_gmt = (date + GMT_OFFSET).strftime(DATE_FORMAT)
         @comment_content = File.read(path)
 
         @post_comments << comment.result(binding)
       end
 
       @header_contents << post.result(binding)
-    rescue Errno::ENOENT
     end
   end
 end
